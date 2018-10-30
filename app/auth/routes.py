e463eb4df26022e349c1fda90fe636bcf8b81542
thatikondasaikrishna <thatikondasaikrishna111@gmail.com>
2018-10-31 02:04:29 +0530

    new download module

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,request , flash , redirect , url_for
+from flask import render_template,request , flash , redirect , url_for,send_file
 
 from flask_login import login_user , logout_user , login_required , current_user
 
@@ -13,6 +13,14 @@ from app import db
 from app.catalog import main
 
 from app.auth.models import EmpDetails , EmpIdscheck
+
+import pandas as pd
+
+from sqlalchemy import create_engine
+
+import datetime
+
+import re
 
 @at.route('/register' , methods=['GET','POST'])
 def register_user():
@@ -83,9 +91,48 @@ def profile_user():
 @at.route('/details/<logout>',methods=['GET','POST'])
 @login_required
 def logout_emps(logout):
+    global filename
     empdetails = EmpDetails.query.all()
+    engine = create_engine('postgresql://postgres:postgres1826@localhost/height_collector')
+    if logout == '22:00:00':
+        query = ''' select emp_id , emp_name,emp_logout,emp_address from empdetails where emp_logout='22:00:00'  '''
+        df = pd.read_sql_query(query, engine)
+    elif logout == '22:30:00':
+        query = ''' select emp_id , emp_name,emp_logout,emp_address from empdetails where emp_logout='22:30:00'  '''
+        df = pd.read_sql_query(query, engine)
+    elif logout == '12:00:00':
+        query = ''' select emp_id , emp_name,emp_logout,emp_address from empdetails where emp_logout='12:00:00'  '''
+        df = pd.read_sql_query(query, engine)
+    elif logout == '12:30:00':
+        query = ''' select emp_id , emp_name,emp_logout,emp_address from empdetails where emp_logout='12:30:00'  '''
+        df = pd.read_sql_query(query, engine)
+    elif logout == '02:00:00':
+        query = ''' select emp_id , emp_name,emp_logout,emp_address from empdetails where emp_logout='02:00:00'  '''
+        df = pd.read_sql_query(query, engine)
+    elif logout == '02:30:00':
+        query = ''' select emp_id , emp_name,emp_logout,emp_address from empdetails where emp_logout='02:30:00'  '''
+        df = pd.read_sql_query(query, engine)
+    elif logout == '04:00:00':
+        query = ''' select emp_id , emp_name,emp_logout,emp_address from empdetails where emp_logout='04:00:00'  '''
+        df = pd.read_sql_query(query, engine)
+    else:
+        query = ''' select emp_id , emp_name,emp_logout,emp_address from empdetails where emp_logout='04:30:00'  '''
+        df = pd.read_sql_query(query, engine)
+    #filename = datetime.datetime.now().strftime("sample_files/%Y-%m-%d-%H-%M-%S-%f" + ".xlsx")
+    filename = datetime.datetime.now().strftime("app/auth/sample_files/%Y-%m-%d-%H-%M-%S-%f" + ".csv")
+    df.to_csv(filename,index=True)
     if current_user.is_authenticated:
-        return render_template('table.html', empdetails=empdetails,logout=logout)
+        return render_template("table.html", empdetails=empdetails,logout=logout, btn='download.html')
+
+
+@at.route("/download-file/")
+def download():
+    #filename1=str(filename)
+    pattern = r"app/"
+    filename1 = re.sub(pattern, "", filename)
+    print(filename1)
+    print(type(filename1))
+    return send_file(filename1, attachment_filename='yourfile.csv', as_attachment=True)
 
 
 @at.app_errorhandler(404)
